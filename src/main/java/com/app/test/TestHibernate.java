17a5a88e38b053692fb6eed9f2215258ed6d355d
suraj <suraj.sunrays86@outlook.com>
2018-08-29 07:45:53 +0530

    Test class is created and Transaction code added.

---
+++ src/main/java/com/app/test/TestHibernate.java
@@ -0,0 +1,15 @@
+package com.app.test;
+
+import org.hibernate.Transaction;
+
+public class TestHibernate {
+    public static void main(String[] args) {
+        Transaction tx = null;
+        try {
+
+        }catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
