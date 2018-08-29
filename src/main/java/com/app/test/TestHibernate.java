b57cff9b7a7f4f06ea8bd1cefa7a73b5b9b6abfd
suraj <suraj.sunrays86@outlook.com>
2018-08-29 07:50:39 +0530

    Test class coding completed.

--- src/main/java/com/app/test/TestHibernate.java
+++ src/main/java/com/app/test/TestHibernate.java
@@ -1,14 +1,45 @@
 package com.app.test;
 
+import com.app.model.Address;
+import com.app.model.Employee;
+import com.app.util.HibernateUtil;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 public class TestHibernate {
     public static void main(String[] args) {
         Transaction tx = null;
-        try {
+        try (Session ss = HibernateUtil.getSf().openSession()){
+            tx = ss.beginTransaction();
+            Address a1 = new Address();
+            a1.setAid(101);
+            a1.setLoc("MUM");
+            a1.setPin(400101);
 
+            Address a2 = new Address();
+            a2.setAid(102);
+            a2.setLoc("KAR");
+            a2.setPin(500102);
+
+            Address a3 = new Address();
+            a3.setAid(103);
+            a3.setLoc("TAL");
+            a3.setPin(600102);
+
+            Employee e = new Employee();
+            e.setEid(10);
+            e.setEname("AA");
+            e.setEsal(12.33);
+            e.getAddr().add(a1);
+            e.getAddr().add(a3);
+
+            ss.save(e);
+
+            tx.commit();
         }catch (Exception e)
         {
+            tx.rollback();
             System.out.println(e.getMessage());
         }
     }
